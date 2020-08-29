b3f262a80d0d761f98cb322bf8d3961d465154d0
mdrews <matthewdrews@gmail.com>
2020-08-29 17:47:12 -0400

    add ImageChangeOnMouseOver for useRef tutorial

---
+++ pages/ImageChangeOnMouseOver.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageChangeOnMouseOver = () => {
+  return (
+    <div>
+      <ImageToggleOnMouseOver primary="/trees/1.webp" 
+                              secondary="/trees/2.webp"
+                              alt="" />
+      &nbsp;&nbsp;&nbsp;
+    </div>
+  )
+}
