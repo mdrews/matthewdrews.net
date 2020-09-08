454cb207da8232c7eef6a147a1cb5d0d81437f48
mdrews <matthewdrews@gmail.com>
2020-09-07 23:57:31 -0400

    add more pages to project

--- pages/ImageChangeOnMouseOver.js
+++ pages/ImageChangeOnMouseOver.js
@@ -1,15 +1,3 @@
 import React from 'react';
-import ImageToggleOnMouseOver from '../src/ImageToggleOnMouseOver';
-
-const ImageChangeOnMouseOver = () => {
-  return (
-    <div>
-      <ImageToggleOnMouseOver primaryImg='static/trees/1.png' 
-                              secondaryImg='static/trees/2.png'
-                              alt="" />
-      &nbsp;&nbsp;&nbsp;
-    </div>
-  );
-};
 
 export default ImageChangeOnMouseOver;
---
+++ pages/testing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TestMail from './testMailer';
+
+const TestPage = () => {
+  return(<div>Test Page!!!</div>);
+};
+
+export default TestPage;
