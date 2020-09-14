673bcb10e4ae2797af95f424c3a757b505f3e505
mdrews <matthewdrews@gmail.com>
2020-09-13 23:37:47 -0400

    kata update to indes.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
-import react, {useState} from 'react';
+import React, { useState } from 'react';
 
-const CaptureTextHistory = () => {
+const MessageTextHistory = () => {
+  
+  const [messageHistory, setMessageHistory] = useState([]);
 
-  const [textHistory, setTextHistory] = useState([]);
-  console.log(textHistory);
-  return(
-    <div>
-      <input
-        onChange={e => setTextHistory([...textHistory, e.target.value])}
-        placeholder="Enter Some Text"
-        value={textHistory[textHistory.length-1]}
-      />
-      <div>{textHistory[textHistory.length-1]}</div>
-      {textHistory.map(message => {
-        return(<div>{message}</div>);
-      })}
-    </div>
-  );
-};
+  return(<div>
+    <input
+      onChange={e => setMessageHistory([...messageHistory, e.target.value])}
+      placeholder='Enter Some Text'
+      value={messageHistory[messageHistory.length-1]}
+    />
+    <div>{messageHistory[messageHistory.length-1]}</div>
+    {messageHistory.map(message => {
+      return(<div>{message}</div>);
+    })}
+  </div>);
+}
 
-export default CaptureTextHistory;
+export default MessageTextHistory;
