c929ed6f8cbac658a3179db37e95c2d49f9a5a35
mdrews <matthewdrews@gmail.com>
2020-09-16 23:59:32 -0400

    kata update ImageToggleOnMouseOver

--- pages/ImageChangeOnMouseOver.js
+++ pages/ImageChangeOnMouseOver.js
@@ -1,3 +1,13 @@
 import React from 'react';
+import ImageToggleOnMouseOver from '../src/ImageToggleOnMouseOver';
+
+const ImageChangeOnMouseOver = () => {
+  return(
+    <ImageToggleOnMouseOver 
+      primary='../static/trees/1.png'
+      secondary='../static/trees/2.png'
+    />
+  )
+}
 
 export default ImageChangeOnMouseOver;
