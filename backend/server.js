e07ea55dc00a1dcf3dac3ba9dee24de033810b10
mdrews <matthewdrews@gmail.com>
2020-11-16 23:53:26 -0500

    add comment to start sever

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.port || 5000;
 
+// Start Server
 app.listen(port, () => {
   console.log(`Server alive and listening at port ${port}`);
 });
