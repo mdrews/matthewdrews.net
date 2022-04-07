8c33c50f6f7c99d20cafa27e7a37f8df6dd0e8b8
mdrews <matthewdrews@gmail.com>
2022-04-07 17:07:09 -0400

    uncomment out code that was broken before

--- pages/Wordle.js
+++ pages/Wordle.js
@@ -16,63 +16,63 @@ function Wordle() {
   let filteredWordle = wordleList;
   
   // REJECTED
-  // for(let x = 0; x < rejected.length; x++) {
-  //   filteredWordle = filteredWordle.filter(word => !word.includes(rejected[x]));
-  // }
-  // let joinedList = filteredWordle.join('');
+  for(let x = 0; x < rejected.length; x++) {
+    filteredWordle = filteredWordle.filter(word => !word.includes(rejected[x]));
+  }
+  let joinedList = filteredWordle.join('');
 
-  // // GREEN
-  // for (let x = 0; x < green.length; x++) {
-  //   let letter = green[x][0];
-  //   let position = green[x][1] - 1;
-  //   filteredWordle = filteredWordle.filter(word => word.includes(letter) && word[position] === letter)
-  // }
-  // // YELLOW
-  // for (let x = 0; x < yellow.length; x++) {
-  //   console.log(filteredWordle)
-  //   let letter = yellow[x][0]
-  //   let position = yellow[x][1] - 1
-  //   console.log(letter)
-  //   console.log(position)
-  //   filteredWordle = filteredWordle.filter(word => word.includes(letter) && word[position] !== letter)
-  // }
-  // // filteredWordle = filteredWordle.filter(word => word.includes('R') && word[3] !== 'R');
-  // let letterGrid = [];
-  // for (let x = 0; x < joinedList.length; x++) {
-  //   let letter = joinedList[x];
-  //   if (joinedList[x] in letterGrid) {
-  //     letterGrid[letter] += 1;
-  //   } else {
-  //     letterGrid[joinedList[x]] = 1;
-  //   }
-  // }
-  // console.log(letterGrid);
-  // const sortable = []
-  // for (let letter in letterGrid) {
-  //   sortable.push([letter, letterGrid[letter]]);
-  // }
-  // sortable.sort(function(a, b) {
-  //   return b[1] - a[1];
-  // });
-  // console.log(`sortable: ${sortable}`)
-  // console.log(sortable[0])
-  // console.log(sortable[1])
-  // console.log(`sortable2: ${sortable[1]}`)
+  // GREEN
+  for (let x = 0; x < green.length; x++) {
+    let letter = green[x][0];
+    let position = green[x][1] - 1;
+    filteredWordle = filteredWordle.filter(word => word.includes(letter) && word[position] === letter)
+  }
+  // YELLOW
+  for (let x = 0; x < yellow.length; x++) {
+    console.log(filteredWordle)
+    let letter = yellow[x][0]
+    let position = yellow[x][1] - 1
+    console.log(letter)
+    console.log(position)
+    filteredWordle = filteredWordle.filter(word => word.includes(letter) && word[position] !== letter)
+  }
+  // filteredWordle = filteredWordle.filter(word => word.includes('R') && word[3] !== 'R');
+  let letterGrid = [];
+  for (let x = 0; x < joinedList.length; x++) {
+    let letter = joinedList[x];
+    if (joinedList[x] in letterGrid) {
+      letterGrid[letter] += 1;
+    } else {
+      letterGrid[joinedList[x]] = 1;
+    }
+  }
+  console.log(letterGrid);
+  const sortable = []
+  for (let letter in letterGrid) {
+    sortable.push([letter, letterGrid[letter]]);
+  }
+  sortable.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+  console.log(`sortable: ${sortable}`)
+  console.log(sortable[0])
+  console.log(sortable[1])
+  console.log(`sortable2: ${sortable[1]}`)
 
-  // let bestList = filteredWordle.filter(word => 
-  //   word.includes(sortable[0][0])
-  //   && word.includes(sortable[1][0])
-  //   // && word.includes(sortable[2][0])
-  //   // && word.includes(sortable[3][0])
-  //   )
+  let bestList = filteredWordle.filter(word => 
+    word.includes(sortable[0][0])
+    && word.includes(sortable[1][0])
+    // && word.includes(sortable[2][0])
+    // && word.includes(sortable[3][0])
+    )
 
-  // console.log('best')
-  // console.log(bestList)
-  // console.log(filteredWordle.filter(word => word.includes(sortable[0][0])))
-  // function updateWordList(e) {
-  //   console.log(`FROM INPUT: ${e.target.value}`);
-  //   return e.target.value;
-  // }
+  console.log('best')
+  console.log(bestList)
+  console.log(filteredWordle.filter(word => word.includes(sortable[0][0])))
+  function updateWordList(e) {
+    console.log(`FROM INPUT: ${e.target.value}`);
+    return e.target.value;
+  }
 
   return (
     <div className="App">
@@ -84,8 +84,8 @@ function Wordle() {
           type="text"
           value="test1"
         />
-        {/* <div>words: {filteredWordle.length}</div>
-        <div>{filteredWordle.join(' ')}</div> */}
+        <div>words: {filteredWordle.length}</div>
+        <div>{filteredWordle.join(' ')}</div>
         {/* <div>{filteredWordle.forEach(word => <p>{word}</p>)}</div> */}
       </header>
     </div>
